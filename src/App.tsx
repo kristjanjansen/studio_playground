6b171ea32bbb9ff136bdcd8243bd45eb5b83dc70
kristjanjansen <kristjan.jansen@gmail.com>
2019-09-15 22:41:23 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,79 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from "react";
+import CardContainer from "./components/CardContainer";
+import DatabrowserContainer from "./components/DatabrowserContainer";
+import DatabrowserHeader from "./components/DatabrowserHeader";
+import Details from "./components/Details";
+import Header from "./components/Header";
+import Table from "./components/Table";
+import TableHeader from "./components/TableHeader";
+import Toolbar from "./components/Toolbar";
+import Variables from "./components/Variables";
+
 import './App.css';
 
-import Layout from  './components/Layout';
+import { randomid } from "./utils";
 
-const App:FC = () => {
+const processData = (data: any) => {
+  return data.map((item: any) => {
+    return {
+      id: randomid(),
+      name: item.name,
+      email: item.email
+    };
+  });
+};
+
+const Loader: FC = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("./data/data.json")
+      .then(res => res.json())
+      .then(data => setData(processData(data)));
+  }, []);
+
   return (
-    <Layout />
+    <div style={{ width: "100%" }}>
+      <div style={{ height: "40px" }}>
+        <Header />
+      </div>
+      <div
+        style={{ display: "flex", height: "calc(100vh - 40px)", zIndex: -100 }}
+      >
+        <div
+          style={{
+            width: "40px",
+            height: "calc(100vh - 40px)"
+          }}
+        >
+          <Toolbar />
+        </div>
+        <div style={{ flex: 1, padding: "10px" }}>
+          <DatabrowserContainer>
+            <div style={{ marginBottom: "10px" }}>
+              <DatabrowserHeader />
+            </div>
+            <CardContainer>
+              <TableHeader data={data} />
+              <Table data={data} />
+            </CardContainer>
+          </DatabrowserContainer>
+        </div>
+        <div style={{ width: "300px", padding: "10px 10px 10px 0" }}>
+          <CardContainer>
+            <Details />
+          </CardContainer>
+        </div>
+      </div>
+      <Variables
+        variables={{
+          "--font-size": { value: 14, min: 8, max: 24 },
+          "--font-size-small": { value: 12, min: 8, max: 24 },
+          "--card-radius": { value: 8, min: 0, max: 24 },
+          "--shadow-blur": { value: 4, min: 0, max: 8 }
+        }}
+      />
+    </div>
   );
-}
+};
 
-export default App;
+export default Loader;
--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,3 @@ export const randomid = (length = 25) => {
   const letters = 'abcdefghijklmnopqrstuvwxyz0123456780'.split('')
   return shuffle(letters).slice(0,length).join('')
 }
-
-export const array2object = array => array.reduce((acc, el) => {
-  acc[el[0]] = el[1]
-  return acc
-}, {})
