37b83982d35bde7a9c625de063092a42db6ef70c
kristjanjansen <kristjan.jansen@gmail.com>
2019-09-19 11:48:14 +0200

    renames

--- src/App2.tsx
+++ src/App2.tsx
@@ -2,17 +2,17 @@ import React, { FC, useState, useEffect } from "react";
 import useWebSocket from "react-use-websocket";
 import PromiseQueue from "easy-promise-queue";
 
-import SelectDb from "./pages/SelectDb";
+// import SelectDb from "./pages/SelectDb";
+// import IntrospectDb from "./pages/IntrospectDb";
+// import SelectKit from "./pages/SelectKit";
+// import SelectTools from "./pages/SelectTools";
+// import GenerateTools from "./pages/GenerateTools";
+// import NextSteps from "./pages/NextSteps";
+//import SelectLanguage from "./pages/SelectLanguage";
 import SetupDb from "./pages/SetupDb";
-import IntrospectDb from "./pages/IntrospectDb";
-import SelectKit from "./pages/SelectKit";
-import SelectTools from "./pages/SelectTools";
-import GenerateTools from "./pages/GenerateTools";
-import NextSteps from "./pages/NextSteps";
 
 import "./App.css";
 import Log from "./components/Log";
-import SelectLanguage from "./pages/SelectLanguage";
 
 let pq = new PromiseQueue({ concurrency: 1 });
 
@@ -42,56 +42,14 @@ const App: FC = () => {
     //}
   }, [logBatch]);
 
-  let [step, setStep] = useState(1);
+  let [step, setStep] = useState(0);
 
   const steps = [
-    // 0
-    <SelectDb
-      onNext={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />,
-    // 1
     <SetupDb
       onPrev={() => setStep(step - 1)}
       onNext={() => setStep(step + 1)}
       onLog={(l: any) => setLogBatch(l)}
     />,
-    // 2
-    <IntrospectDb
-      onPrev={() => setStep(step - 1)}
-      onDone={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />,
-    // 3
-    <SelectLanguage
-      onPrev={() => setStep(step - 2)}
-      onNext={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />,
-    // 4
-    <SelectTools
-      onPrev={() => setStep(step - 1)}
-      onNext={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />,
-    // 5
-    <SelectKit
-      onPrev={() => setStep(step - 1)}
-      onNext={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />,
-    // 6
-    <GenerateTools
-      onPrev={() => setStep(step - 1)}
-      onDone={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />,
-    // 7
-    <NextSteps
-      onPrev={() => setStep(step - 2)}
-      onNext={() => setStep(step + 1)}
-      onLog={(l: any) => setLogBatch(l)}
-    />
   ];
 
   return (
